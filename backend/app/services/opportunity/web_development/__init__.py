d72ee4cbd566280426f1e7dd745fb28209ec30f3
Murillo Lourenço <murilloalveslou@outlook.com>
2026-08-14 15:25:38 -0300

    chore: export web development opportunity module

---
+++ backend/app/services/opportunity/web_development/__init__.py
@@ -0,0 +1,6 @@
+from app.services.opportunity.web_development.module import (
+    WEB_OPPORTUNITY_VERSION,
+    WebDevelopmentOpportunityModule,
+)
+
+__all__ = ["WEB_OPPORTUNITY_VERSION", "WebDevelopmentOpportunityModule"]
